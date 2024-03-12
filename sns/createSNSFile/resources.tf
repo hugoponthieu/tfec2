2b46aa2b47170682b2101cf86d2a55b500969a7d
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-03-12 17:25:22 +0100

    changing var and route

--- sns/createSNSFile/resources.tf
+++ sns/createSNSFile/resources.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "notifier" {
 resource "aws_sns_topic_subscription" "add_email" {
   topic_arn = aws_sns_topic.notifier.arn
   protocol  = "email"
-  endpoint= hugop
+  endpoint  = var.email
 }
--- sns/createSNSFile/variables.tf
+++ sns/createSNSFile/variables.tf
@@ -1,4 +1,3 @@
 variable "email" {
   type    = string
-  default = "hugopont08@gmail.com"
 }
