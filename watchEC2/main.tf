fe39e7b6d16632b733b68129566acafa6f0397a0
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-03-25 21:30:09 +0100

    adding props watch ec2

--- watchEC2/main.tf
+++ watchEC2/main.tf
@@ -1,16 +1,17 @@
 module "sns" {
-  source = "../sns/createSNSFile"
-  emails =  var.emails
+  source   = "../sns/createSNSFile"
+  emails   = var.emails
   sns_name = var.sns_name
 }
 module "lambda" {
-  source = "../sns/createLambda"
-  sns_arn = module.sns.sns_arn_out
+  source              = "../sns/createLambda"
+  sns_arn             = module.sns.sns_arn_out
   sended_notification = var.sended_notification
-  lambda_name = var.lambda_name
-  lambda_role_name = var.lambda_role_name
+  lambda_name         = var.lambda_name
+  lambda_role_name    = var.lambda_role_name
 }
 module "event_rule" {
-  source = "./event_rule"
-  lambda_arn = module.lambda.lambda_arn
+  source      = "./event_rule"
+  lambda_arn  = module.lambda.lambda_arn
+  lambda_name = var.lambda_name
 }
