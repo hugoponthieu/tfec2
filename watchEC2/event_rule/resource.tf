f1b37dcaf39b5ce18257a50562d4b63378766a8e
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-03-25 21:11:24 +0100

    fix

--- watchEC2/event_rule/resource.tf
+++ watchEC2/event_rule/resource.tf
@@ -19,5 +19,5 @@ resource "aws_lambda_permission" "allow_eventbridge" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.ec2_state_change.arn
+  source_arn    = aws_cloudwatch_event_rule.console.arn
 }
